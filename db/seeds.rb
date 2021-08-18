6a36c7359cb1d202909d14c245a180ff31fd4061
Drew Jennings <andrew_jennings@ymail.com>
2021-08-18 16:00:53 +0100

    add index page with all flats

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  Flat.create!(name: Faker::TvShows::Simpsons.location,
+              address: Faker::Address.street_address,
+              description: Faker::Movies::StarWars.quote,
+              price_per_night: rand(50..150),
+              number_of_guests: rand(1..10))
+end
+
+puts "Seeded #{Flat.count} flats!"
