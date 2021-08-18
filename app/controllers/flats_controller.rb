c7488bc68e8e8eb94fe908456321849508a39c45
Drew Jennings <andrew_jennings@ymail.com>
2021-08-18 16:35:30 +0100

    add edit function for flat

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -21,9 +21,16 @@ class FlatsController < ApplicationController
   end
 
   def edit
+    @flat = Flat.find(params[:id])
   end
 
   def update
+    @flat = Flat.find(params[:id])
+    if @flat.update(flat_params)
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
   end
 
   def destroy
